b260a3b0c4ef36fc0c024b9557e6f2c57320210b
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-20 14:51:33 -0500

    Add project files.

---
+++ BlogSpotMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogSpotMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
